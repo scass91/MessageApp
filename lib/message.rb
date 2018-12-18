dd636856debfce670a4d30eaf3a6c6c3186c99da
Simon Cass <scass1991@gmail.com>
2018-12-18 16:02:16 +0000

    Added test-driven message class

---
+++ lib/message.rb
@@ -0,0 +1,9 @@
+class Message
+
+  attr_reader :message
+
+  def initialize(message)
+    @message = message
+  end
+
+end
---
+++ spec/message_spec.rb
@@ -0,0 +1,8 @@
+require "./lib/message.rb"
+
+describe "message" do
+  it "it exists" do
+    m = Message.new("Hello")
+    expect(m.message).to eq("Hello")
+  end
+end
