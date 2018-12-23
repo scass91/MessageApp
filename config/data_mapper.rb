aece5d53e48829f0ba77758eb2af09e7f3783cce
Simon Cass <scass1991@gmail.com>
2018-12-23 16:43:16 +0000

    Added database selector

--- config/data_mapper.rb
+++ config/data_mapper.rb
@@ -4,4 +4,6 @@ DataMapper.setup(:default, "postgres://localhost/messageapp_#{ENV["ENV_RACK"]}")
 
 DataMapper.finalize
 
-DataMapper.auto_migrate!
+DataMapper.auto_upgrade! if ENV['RACK_ENV'] == 'development'
+
+DataMapper.auto_migrate! if ENV['RACK_ENV'] == 'test'
