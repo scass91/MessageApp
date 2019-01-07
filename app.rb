ee6ef67f224e347382e5626984478e0785843a0b
Simon Cass <scass1991@gmail.com>
2019-01-07 10:40:53 +0000

    Cleaned up with rubocop

--- app.rb
+++ app.rb
@@ -6,7 +6,6 @@ require './config/data_mapper'
 
 require 'pry'
 
-
 class FACESMASH < Sinatra::Base
   enable :sessions
   enable :method_override
@@ -30,12 +29,6 @@ class FACESMASH < Sinatra::Base
     @message = Message.get(params[:id])
     erb(:edit)
   end
-
-  # Hello reviewers! My understanding of "PATCH" comes from the below link
-  # http://restcookbook.com/HTTP%20Methods/patch/
-  # https://wixelhq.com/blog/working-with-sinatra-routes-and-conditions
-  # As far as I can see, it's not perfect, but it's useful for updating small parts of a given
-  # Data set, for example, changing one entry in a messaging app!
 
   post '/message/:id' do |id|
     @message = Message.get!(id.to_i)
